570e5192803e3e4949ac951d9145f1e796f6f244
Jung Minji <52596617+mjjunng@users.noreply.github.com>
2022-03-28 22:31:30 +0900

    Update 문제모음.py

--- Algorithm/dp/문제모음.py
+++ Algorithm/dp/문제모음.py
@@ -63,3 +63,60 @@ if __name__ == "__main__":
         res[i] = increase[i] + decrease[i] - 1  # 중복 숫자빼줘야 해서 -1함
 
     print(max(res))
+    
+    
+    '''
+    11048 
+    
+    생각의 전환! 다음 위치로 갈 수 있는 경로가 (r+1, c), (r, c+1), (r+1, c+1) 이면,
+    현재 위치로 올 수 있는 전 위치는 (r-1, c), (r, c-1), (r-1, c-1) 이다!!!
+
+
+'''
+
+if __name__ == "__main__":
+    sys.stdin = open("input.txt", "rt")
+    n, m = map(int, input().split())
+
+    board = []
+    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    for i in range(n):
+        board.append(list(map(int, input().split())))
+
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            dp[i][j] = board[i-1][j-1] + max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+            # 현재 위치에 있는 사탕의 개수 + 이전 위치에 있던 사탕의 수 중 max값
+
+    print(dp[n][m])
+    
+    
+'''
+    1309 동물원
+    
+    
+    점화식 : 
+    N = 2인 곳에 아무것도 배치 x 경우 : dp[i-1] (전과 동일)
+    N = 1인 곳에 아무것도 배치 x 경우 : dp[i-2] * 2
+    N = 1, 2에 모두 배치 : dp[i-1] - dp[i-2]
+    
+'''
+
+
+
+if __name__ == "__main__":
+    sys.stdin = open("input.txt", "rt")
+    n = int(input())
+
+    dp = [0 for _ in range(n+1)]
+
+    dp[0] = 1
+    dp[1] = 3
+
+    for i in range(2, n+1):
+        dp[i] = (dp[i-2] + dp[i-1] * 2) % 9901
+
+
+    print(dp[n])
+
