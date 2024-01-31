f88131ad9c86e38a0e71f72744c80fa8e1852450
mjjunng <52596617+mjjunng@users.noreply.github.com>
2024-02-01 00:48:47 +0900

    Create 피보나치 수.py

---
+++ Algorithm/Programmers/Lv2/피보나치 수.py
@@ -0,0 +1,11 @@
+def solution(n):
+    fibo = [0 for _ in range(n+1)]
+    fibo[1] = 1
+    
+    if n < 2:
+        return fibo[n]
+    
+    for i in range(2, n+1):
+        fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567
+        
+    return fibo[n]
