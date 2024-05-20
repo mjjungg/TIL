3bc8e8c6a6b975fef97a996888dc71fe65087c3a
mjjunng <52596617+mjjunng@users.noreply.github.com>
2024-05-20 22:25:21 +0900

    dijkstra

--- Algorithm/Programmers/Lv2/배달.py
+++ Algorithm/Programmers/Lv2/배달.py
@@ -26,3 +26,34 @@ def solution(N, road, K):
             answer += 1
                 
     return answer
+
+
+## 다익스트라
+import heapq
+
+def solution(N, road, K):
+    answer = 0
+    hq = []
+    graph = [[] for _ in range(N+1)]
+    dis = [float('inf') for _ in range(N+1)]
+    dis[1] = 0
+    
+    for a, b, c in road:
+        graph[a].append([b, c])
+        graph[b].append([a, c])
+    
+    heapq.heappush(hq, [0, 1])
+    
+    while hq:
+        v, node = heapq.heappop(hq)
+        
+        for b, c in graph[node]:
+            if dis[node] + c < dis[b]:
+                dis[b] = dis[node] + c
+                heapq.heappush(hq, [dis[b], b])
+    
+    for i in range(1, len(dis)):
+        if dis[i] <= K:
+            answer += 1
+                
+    return answer
