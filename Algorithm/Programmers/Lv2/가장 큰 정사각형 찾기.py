ff4d079152ecc1ee5cb13e7d44de6cbd34a20500
mjjunng <52596617+mjjunng@users.noreply.github.com>
2024-05-31 23:56:03 +0900

    dp

---
+++ Algorithm/Programmers/Lv2/가장 큰 정사각형 찾기.py
@@ -0,0 +1,21 @@
+def solution(board):
+    answer = 0
+    n = len(board)
+    m = len(board[0])
+    dp = [[0 for _ in range(m)] for _ in range(n)]
+    
+    for i in range(n):
+        dp[i][0] = board[i][0]
+    
+    for i in range(m):
+        dp[0][i] = board[0][i]
+    
+    for i in range(1, n):
+        for j in range(1, m):
+            if board[i][j] == 1:
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+    
+    for k in dp:
+        answer = max(answer, max(k))
+                                    
+    return answer * answer
