c096efb5895df5ccee81d641e20be9fa5fde4386
mjjungg <mjjunghp@naver.com>
2025-02-27 22:05:00 +0900

    Update 7576_토마토.py

--- Algorithm/dfs/7576_토마토.py
+++ Algorithm/dfs/7576_토마토.py
@@ -48,3 +48,63 @@ if __name__ == "__main__":
 
     print(cnt)
 
+
+'''
+    2025.02.27
+    사용한 풀이: bfs
+    
+'''
+import sys
+from collections import deque
+
+
+def check(lst):
+    for i in range(len(lst)):
+        for j in range(len(lst[0])):
+            if lst[i][j] == 0:
+                return False
+    return True
+
+if __name__ == "__main__":
+    sys.stdin = open("input.txt", "r")
+    m, n = map(int, input().split())
+
+    tomatos = []
+    visited = [[0 for _ in range(m)] for _ in range(n)]
+    q = deque()
+    dy = [0, 0, 1, -1]
+    dx = [1, -1, 0, 0]
+
+    result = 0
+
+    for i in range(n):
+        lst = list(map(int, input().split()))
+        tomatos.append(lst)
+        for j in range(m):
+            if tomatos[i][j] == 1:
+                q.append((i, j))
+
+    if check(tomatos):
+        print(0)
+        sys.exit()
+
+    while q:
+        y, x = q.popleft()
+
+        for i in range(len(dy)):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if (0 <= ny < n) and (0 <= nx < m):
+                if tomatos[ny][nx] == 0 and visited[ny][nx] == 0:
+                    q.append((ny, nx))
+                    visited[ny][nx] = visited[y][x] + 1
+                    tomatos[ny][nx] = 1
+
+    if check(tomatos):
+        for k in visited:
+            result = max(result, max(k))
+
+        print(result)
+    else:
+        print(-1)
